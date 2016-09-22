9c8edece6e434313e1684174df638f47af464a8e
Leandro Maduro <leandro.maduro@walmart.com>
2016-09-22 13:53:32 -0300

    Set name for subjects

--- spec/rack/healthcheck/actions/complete_spec.rb
+++ spec/rack/healthcheck/actions/complete_spec.rb
@@ -3,10 +3,10 @@ require "spec_helper"
 describe Rack::Healthcheck::Actions::Complete do
   describe ".new" do
     describe "with invalid request method" do
-      subject { described_class.new("/path", "POST") }
+      subject(:action) { described_class.new("/path", "POST") }
 
       it "raises InvalidRequestMethod exception" do
-        expect{subject}.to raise_error(Rack::Healthcheck::Actions::Base::InvalidRequestMethod, "Method not allowed")
+        expect{ action }.to raise_error(Rack::Healthcheck::Actions::Base::InvalidRequestMethod, "Method not allowed")
       end
     end
   end
--- spec/rack/healthcheck/actions/load_balancer_spec.rb
+++ spec/rack/healthcheck/actions/load_balancer_spec.rb
@@ -3,68 +3,67 @@ require "spec_helper"
 describe Rack::Healthcheck::Actions::LoadBalancer do
   describe ".new" do
     describe "with invalid request method" do
-      subject { described_class.new("/path", "PATH") }
+      subject(:action) { described_class.new("/path", "PATH") }
 
       it "raises InvalidRequestMethod exception" do
-        expect{subject}.to raise_error(Rack::Healthcheck::Actions::Base::InvalidRequestMethod, "Method not allowed")
+        expect{ action }.to raise_error(Rack::Healthcheck::Actions::Base::InvalidRequestMethod, "Method not allowed")
       end
     end
   end
 
   describe "#get" do
     describe "when status is LIVE" do
-      subject(:check) { described_class.new("/healthcheck", "get") }
+      subject(:action) { described_class.new("/healthcheck", "get") }
 
       it 'returns LIVE' do
-        expect(check.get).to include(["LIVE"])
+        expect(action.get).to include(["LIVE"])
       end
     end
 
     describe "when status is DEAD" do
-      subject(:check) { described_class.new("/healthcheck", "get") }
+      subject(:action) { described_class.new("/healthcheck", "get") }
 
       before do
         Rack::Healthcheck::Actions::LoadBalancer.status = Rack::Healthcheck::Actions::LoadBalancer::DEAD
       end
 
       it 'returns DEAD' do
-        expect(check.get).to include(["DEAD"])
+        expect(action.get).to include(["DEAD"])
       end
     end
-
   end
 
   describe "#post" do
-    subject(:check) { described_class.new("/healthcheck", "post") }
+    subject(:action) { described_class.new("/healthcheck", "post") }
 
     before(:each) do
       Rack::Healthcheck::Actions::LoadBalancer.status = Rack::Healthcheck::Actions::LoadBalancer::DEAD
     end
 
     it 'returns LIVE' do
-      expect(check.post).to include(["LIVE"])
+      expect(action.post).to include(["LIVE"])
     end
 
     it 'changes variable value to LIVE' do
-      check.post
+      action.post
 
       expect(Rack::Healthcheck::Actions::LoadBalancer.status).to eq(Rack::Healthcheck::Actions::LoadBalancer::LIVE)
     end
   end
 
   describe "#delete" do
-    subject(:check) { described_class.new("/healthcheck", "delete") }
+    subject(:action) { described_class.new("/healthcheck", "delete") }
 
     before(:each) do
       Rack::Healthcheck::Actions::LoadBalancer.status = Rack::Healthcheck::Actions::LoadBalancer::LIVE
     end
 
     it 'returns DEAD' do
-      expect(check.delete).to include(["DEAD"])
+      expect(action.delete).to include(["DEAD"])
     end
 
     it 'changes variable value to DEAD' do
-      check.delete
+      action.delete
 
       expect(Rack::Healthcheck::Actions::LoadBalancer.status).to eq(Rack::Healthcheck::Actions::LoadBalancer::DEAD)
     end
