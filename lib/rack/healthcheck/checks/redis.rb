3dc6d485842a11e7313f03f11e4b55de3d718dbd
Leandro Maduro <leandro.maduro@walmart.com>
2016-09-22 16:58:21 -0300

    Create check for redis

---
+++ lib/rack/healthcheck/checks/redis.rb
@@ -0,0 +1,34 @@
+require "rack/healthcheck/checks/base"
+require "rack/healthcheck/type"
+
+module Rack::Healthcheck::Checks
+  class Redis < Base
+    attr_reader :config
+
+    # @param name [String]
+    # @param config [Hash<Symbol,String>]
+    # @param optional [Boolean] Flag used to inform if this service is optional
+    # @example
+    # name = Redis
+    # config = {
+    #   url: "redis://localhost:6379",
+    #   password: "pass"
+    # }
+    # optional = true
+    def initialize(name, config, optional = false)
+      super(name, optional, config[:url])
+      @type   = Rack::Healthcheck::Type::CACHE
+      @config = config
+    end
+
+    private
+
+    def check
+      redis = ::Redis.new(config)
+      redis.info
+      @status = true
+    rescue Exception => e
+      @status = false
+    end
+  end
+end
---
+++ spec/rack/healthcheck/checks/redis_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require "rack/healthcheck/type"
+
+describe Rack::Healthcheck::Checks::Redis do
+  let(:config) do
+    {
+      url: "localhost",
+      password: 6379,
+    }
+  end
+  let(:redis_check) { Rack::Healthcheck::Checks::Redis.new("name", config) }
+
+  before(:each) do
+    redis = Class.new
+    redis.class_eval { def initialize(config); end; def info; end }
+    stub_const("Redis", redis)
+  end
+
+  describe ".new" do
+    it "sets type as CACHE" do
+      expect(redis_check.type).to eq(Rack::Healthcheck::Type::CACHE)
+    end
+  end
+
+  describe "#run" do
+    subject(:run_it) { redis_check.run }
+
+    describe "when redis server is available" do
+      it "sets status to true" do
+        run_it
+
+        expect(redis_check.status).to be_truthy
+      end
+    end
+
+    describe "when redis server is down" do
+      it "sets status to false" do
+        allow_any_instance_of(Redis).to receive(:info).and_raise(Exception)
+        run_it
+
+        expect(redis_check.status).to be_falsy
+      end
+    end
+
+    it "sets elapsed_time" do
+      run_it
+
+      expect(redis_check.elapsed_time).to_not be_nil
+    end
+  end
+end
