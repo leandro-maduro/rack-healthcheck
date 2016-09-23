476c518c280a802e3e86a130e14e97e3c859a9bc
Leandro Maduro <leandro.maduro@walmart.com>
2016-09-22 22:06:09 -0300

    Implement tests for configuration class

--- spec/rack/healthcheck_spec.rb
+++ spec/rack/healthcheck_spec.rb
@@ -4,4 +4,34 @@ describe Rack::Healthcheck do
   it 'has a version number' do
     expect(Rack::Healthcheck::VERSION).not_to be nil
   end
+
+  describe ".configure" do
+    subject(:configure) do
+      Rack::Healthcheck.configure do |config|
+        config.app_name     = "test"
+        config.app_version  = 1.0
+        config.checks       = [
+          Rack::Healthcheck::Checks::ActiveRecord.new("database")
+        ]
+      end
+    end
+
+    it "sets app_name" do
+      configure
+
+      expect(Rack::Healthcheck.configuration.app_name).to eq("test")
+    end
+
+    it "sets app_version" do
+      configure
+
+      expect(Rack::Healthcheck.configuration.app_version).to eq(1.0)
+    end
+
+    it "sets checks" do
+      configure
+
+      expect(Rack::Healthcheck.configuration.checks.size).to eq(1)
+    end
+  end
 end
