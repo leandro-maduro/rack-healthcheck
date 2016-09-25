5c498823618ccbe268cb172c966be98847cbf97b
Leandro Maduro <leandro.maduro@walmart.com>
2016-09-25 01:07:14 -0300

    Add documentation on configuration class

--- lib/rack/healthcheck/configuration.rb
+++ lib/rack/healthcheck/configuration.rb
@@ -1,4 +1,16 @@
 module Rack::Healthcheck
+  # This class is responsible by keep all services that the healthcheck needs to verify
+  #
+  # app_name [String] This value is used only in complete check result
+  # app_version [String] This value is used only in complete check result
+  # checks [Array] Array that contains all services that healthcheck needs to verify
+  # @example
+  # app_name = "Rack Healthcheck"
+  # app_version = 1.0
+  # checks = [
+  #   Rack::Healthcheck::Checks::ActiveRecord.new("database"),
+  #   Rack::Healthcheck::Checks::Redis.new("redis", {})
+  # ]
   class Configuration
     attr_accessor :app_name, :app_version, :checks
 
